fcbc3cf5237dfcbbd7f720b53b733a10a5b67e25
James <admin@James.local>
2020-04-18 23:33:50 +0300

    chore(Tests): Test articles
    - create a root tests folder
    - create tests folder for articles
    - template the tests for an article
    
    [Finishes #172393750]

--- tests/article_tests/ArticlesHandlers.spec.js
+++
@@ -1,8 +0,0 @@
-describe("Articles", () => {
-  it("should check whether an article was created successfully", () => {
-    expect(true).toBe(true);
-  });
-  it("should check whether an article was created successfully", () => {
-    expect(false).toBe(false);
-  });
-});
